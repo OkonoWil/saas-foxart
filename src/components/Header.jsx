572e5ae011262187a60b501a5712906b197df561
OkonoWil <okonowilfried@gmail.com>
2024-12-06 19:56:36 +0100

    add asset

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+
+export const Header = () => {
+    return (
+        <div className="border-b border-stroke-1">
+            <img src={logoApp} width={104} height={32} alt="App Logo" />
+        </div>
+    )
+}
